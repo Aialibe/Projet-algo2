6c2df29ca13e5873759eba6cfd049d1110a8fc98
Fanchoir302 <clem.jourdinn@gmail.com>
2025-01-06 11:40:06 +0100

    Update demineur.c

--- demineur.c
+++ demineur.c
@@ -3,5 +3,29 @@
 #define MINES 20
 #define GRILLE 10
 
+int trouver_identifiant_depuis_position(int x, int y){
+  //Renvoie l'identifiant correspondant à la case (x,y) du plateau
+  //Identifiants comptés de gauche à droite puis de haut en bas
+  return x + y*GRILLE;
+}
 
-patrick
+int trouver_position_x_depuis_identifiant(int identifiant){
+  return identifiant%(GRILLE);
+}
+
+int trouver_position_y_depuis_identifiant(int identifiant){
+  return identifiant/(GRILLE);
+}
+
+void initialiser(char** plateau){
+    int i = 0;
+    while(i < MINES){
+        mine = rand() % GRILLE*GRILLE;
+        x = trouver_position_x_depuis_identifiant(mine);
+        y = trouver_position_y_depuis_identifiant(mine);
+        if(plateau[x][y] != "M"){
+            plateau[x][y] = "M";
+            i++;
+        }
+    }
+}
