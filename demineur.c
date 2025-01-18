d2d1eca7e29c085b1d6ab8be7cd3fd3f763de0ed
Aialibe <helene.rouchouze@gmail.com>
2025-01-18 23:34:59 +0100

    Update demineur.c

--- demineur.c
+++ demineur.c
@@ -640,7 +640,7 @@ void creer_arbre_decision(ARBRE a, int TAILLE_ELT){
 
 bool est_possible_situation(ELT m, int TAILLE_ELT){
     //Renvoie true si la situation décrite pas m est bien définie et false sinon
-    rand (time(NULL));
+   
     bool res = true;
     for(int x = 0; x < TAILLE_ELT; x++){
         for(int y = 0; y < TAILLE_ELT; y++){
@@ -808,6 +808,7 @@ void ia(char** visible, int x_depart, int y_depart){
 
 
 void fonction_principale(){
+    srand (time(NULL))
     bool perdu = false;
     bool gagne = false;
     int compteur_mines= MINES;
